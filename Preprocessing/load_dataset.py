739c607cb7ef006f75bad1969cdc97a4e2c6b177
Giulio <72512193+BaffoBello14@users.noreply.github.com>
2023-01-25 16:22:56 +0100

    Sistemato il codice

--- Preprocessing/load_dataset.py
+++ Preprocessing/load_dataset.py
@@ -6,18 +6,16 @@ numerical_columns = []
 
 def import_dataset():
     df1 = pd.read_csv("../Dataset/vehicles10.csv")
-    df1.drop('county', axis=1, inplace=True)
+    df1.drop('county', axis=1, inplace=True) 
 
     df2 = pd.read_csv("../Dataset/vehicles8.csv")
     df2.drop('Unnamed: 0', axis=1, inplace=True)
 
     df = pd.merge(df1, df2, how="outer")
 
-    df['posting_date'] = pd.to_datetime(df['posting_date'])
-    df['age'] = df['posting_date'].dt.year - df['year']
-
-    df.drop("posting_date", axis=1, inplace=True)
-    df.drop("year", axis=1, inplace=True)
+    #df.drop("posting_date", axis=1, inplace=True)
+    #df.drop("year", axis=1, inplace=True)
+    
     return df
 
 
@@ -25,19 +23,10 @@ def import_dataset():
 
 
 def delete_useless_columns(df):
-    df.drop('id', axis=1, inplace=True)
-    df.drop('url', axis=1, inplace=True)
-    df.drop('region_url', axis=1, inplace=True)
-    df.drop('VIN', axis=1, inplace=True)
-    df.drop('size', axis=1, inplace=True)
-    df.drop('image_url', axis=1, inplace=True)
-    df.drop('description', axis=1, inplace=True)
-    df["condition"].replace(pd.np.nan, 'good', inplace=True)
-    df.dropna(
-        subset=['price', 'odometer', 'year', 'manufacturer', 'cylinders', 'fuel', 'transmission', 'drive', 'type'],
-        inplace=True)
+    df.drop(['id', 'url', 'region_url', 'VIN', 'size', 'image_url', 'description'], axis=1, inplace=True)
+    df["condition"].replace(np.nan, 'good', inplace=True)
+    df.dropna(subset=['price', 'odometer', 'year', 'manufacturer', 'cylinders', 'fuel', 'transmission', 'drive', 'type'], inplace=True)
     # Eliminando tutti i NULL si raggiunge circa lo 0.65 di accuracy (RF)
-
 
 def split_categorical_numerical(df):
     for column in df:
