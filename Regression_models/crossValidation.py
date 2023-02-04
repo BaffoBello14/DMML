f1ae9b5e76332b48f4bf7b962ebd43ce03edc0ac
fratifederico <121012872+fratifederico@users.noreply.github.com>
2023-02-04 11:56:19 +0100

    Update crossValidation.py
    corretti

--- Regression_models/crossValidation.py
+++ Regression_models/crossValidation.py
@@ -4,6 +4,11 @@ from sklearn.linear_model import Lasso, ElasticNet
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import KFold
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
+
 
 
 def crossValRandomForest(df):
@@ -14,7 +19,7 @@ def crossValRandomForest(df):
 
     # Inizializza la cross-validation k-fold con 10 divisioni
     kfold = KFold(n_splits=10, random_state=42, shuffle=True)
-
+    print('results for RF')
     # Inizia la cross-validation
     for train_index, test_index in kfold.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
@@ -37,7 +42,7 @@ def crossValLassoAlg(df):
 
     # Use K-Fold cross-validation to evaluate the model performance
     kf = KFold(n_splits=10, shuffle=True, random_state=42)
-
+    print('Results for Lasso')
     for train_index, test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -47,6 +52,8 @@ def crossValLassoAlg(df):
         lasso.fit(X_train, y_train)
         predict_y = lasso.predict(X_test)
         score = lasso.score(X_test, y_test)
+        mse = mean_squared_error(y_test, predict_y)
+        print("Mean square error:", mse)
         print("Accuracy:", score)
 
 
@@ -56,7 +63,7 @@ def crossValAdaBoostRegressor(df):
 
     # Use K-Fold cross-validation to evaluate the model performance
     kf = KFold(n_splits=10, shuffle=True, random_state=42)
-
+    print('Results for Adaboost')
     for train_index, test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -66,6 +73,8 @@ def crossValAdaBoostRegressor(df):
         ada.fit(X_train, y_train)
         predict_y = ada.predict(X_test)
         score = ada.score(X_test, y_test)
+        mse = mean_squared_error(y_test, predict_y)
+        print("Mean square error:", mse)
         print("Accuracy:", score)
 
 
@@ -75,7 +84,7 @@ def crossValElasticNet(df):
 
     # Use K-Fold cross-validation to evaluate the model performance
     kf = KFold(n_splits=10, shuffle=True, random_state=42)
-
+    print('Results for ElasticNet')
     for train_index, test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -84,11 +93,67 @@ def crossValElasticNet(df):
         model.fit(X_train, y_train)
         predict_y = model.predict(X_test)
         score = model.score(X_test, y_test)
+        mse = mean_squared_error(y_test, predict_y)
+        print("Mean square error:", mse)
         print("Accuracy:", score)
 
 
+def crossValKNN(df):
+    X = df.drop("price", axis=1)
+    y = df["price"]
+    # Inizializza il classificatore k-NN
+    model = KNeighborsRegressor(n_neighbors=5)
+
+    # Inizializza la cross-validation k-fold con 10 divisioni
+    kfold = KFold(n_splits=10, random_state=42, shuffle=True)
+    print('results for 5-NN')
+    # Inizia la cross-validation
+    for train_index, test_index in kfold.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # Addestra il modello sul set di training
+        model.fit(X_train, y_train)
+
+        # Valuta le prestazioni sul set di test
+        score = model.score(X_test, y_test)
+        predict_y = model.predict(X_test)
+        mse = mean_squared_error(y_test, predict_y)
+        print("Mean square error:", mse)
+        print("Accuracy:", score)
+
+
+
+def crossValM5Rules(df):
+    X = df.drop("price", axis=1)
+    y = df["price"]
+    # Inizializza il classificatore Decision Tree
+    model = DecisionTreeRegressor(criterion="mse", random_state=42)
+
+    # Inizializza la cross-validation k-fold con 10 divisioni
+    kfold = KFold(n_splits=10, random_state=42, shuffle=True)
+    print('results for M5rules')
+    # Inizia la cross-validation
+    for train_index, test_index in kfold.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # Addestra il modello sul set di training
+        model.fit(X_train, y_train)
+
+        # Valuta le prestazioni sul set di test
+        predict_y = model.predict(X_test)
+        mse = mean_squared_error(y_test, predict_y)
+        print("Mean square error:", mse)
+        print("Accuracy:", model.score(X_test, y_test))
+
+
+
+
 df = pd.read_csv("../Dataset/vehicles_preprocessed.csv")
-#crossValAdaBoostRegressor(df)
-#crossValElasticNet(df)
-#crossValLassoAlg(df)
+#crossValAdaBoostRegressor(df)  #circa 0.47
+#crossValElasticNet(df)  #circa 0.5
+#crossValLassoAlg(df)    #circa 0.5
+#crossValM5Rules(df)     #circa 0.61
+#crossValKNN(df)         #circa 0.35
 crossValRandomForest(df)
